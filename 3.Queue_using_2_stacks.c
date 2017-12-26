0397b5870a7efc219ad90f0997578bf74fa9f562
Amal Poulose <amalpoulose1995@hotmail.com>
2017-12-26 16:36:07 +0530

    Update 3.Queue_using_2_stacks.c

--- 3.Queue_using_2_stacks.c
+++ 3.Queue_using_2_stacks.c
@@ -59,7 +59,7 @@ void enqueue(Queue *ptr,int n)
 		printf("Queue Full\n");
 		return;
 	}
-	push(&ptr->stack1,n);
+	push(&ptr->stack1,n); //when an element is queued it will push to stack1
 	printf("%d is enqueued\n",n);
 	size++;
 }
@@ -73,13 +73,13 @@ void dequeue(Queue *ptr)
 		return;
 	}
 	if(ptr->stack2==0)
-		while(ptr->stack1!=0)
+		while(ptr->stack1!=0)//pop values frome stack 1 and pushes to stack2 until stack 1 become empty
 		{
 			x=pop(&ptr->stack1);
 			push(&ptr->stack2,x);
 		}
-	printf("%d is dequeued \n",pop(&ptr->stack2));
-	while(ptr->stack2!=0)
+	printf("%d is dequeued \n",pop(&ptr->stack2)); //pop a value from stack2 which is the dequeued value
+	while(ptr->stack2!=0)    //pop back all values from stack 2 and push to stack 1
 	{
 		x=pop(&ptr->stack2);
 		push(&ptr->stack1,x);
@@ -87,7 +87,7 @@ void dequeue(Queue *ptr)
 	size--;
 }
 
-void push(node **ptr,int n)
+void push(node **ptr,int n)    //add_begin logic of linkedlist
 {
 	node *new=malloc(sizeof(node*)); 
 	new->data=n;
@@ -100,9 +100,9 @@ int pop(node **ptr)
 	node *temp;
 	int x;
 	temp=*ptr;
-	x=temp->data;
-	*ptr=(*ptr)->next;
-	free(temp);
+	x=temp->data; //store current head pointer value temp
+	*ptr=(*ptr)->next;//increment headpointer value to next node
+	free(temp);//free the temp pointer 
 	return x;
 }
 
